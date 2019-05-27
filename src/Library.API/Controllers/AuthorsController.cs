d580fe3d4a3e219c3a626a4c95b8b94846dab332
kosenburg <kosenburg@gmail.com>
2019-05-26 22:46:11 -0400

    Added first action to access authors resource.

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")]
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = libraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+
+        }
+    }
+}
